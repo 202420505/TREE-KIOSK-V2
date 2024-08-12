2798f78bcc9facc8f6184e149df02894520005dd
202420505 <202420505@eulji.ms.kr>
2024-08-12 14:57:31 +0900

    update

--- main.js
+++ main.js
@@ -1,58 +1,17 @@
 const ul = document.getElementById("list");
 
-window.addEventListener('load', init);
-
-function init() {
-  home();
-    if (ul && ul.innerHTML.trim() === '') {
-      loaditem();
-    }
-  }
-
-
-function loaditem() {
-  fetch("/image/file.json")
-  .then(res => res.json())
-  .then(data => {
-    let dataMarkup = '';
-
-    for(key in data) {
-      dataMarkup += display(data[key],key);
-      } 
-
-      ul.innerHTML = dataMarkup; 
-}
-  )
-}
-
-function display(child,key) {
-
-  return `
- <li class="list-group-item d-flex justify-content-between align-items-center item">
-      <img class="lazyload"src="${child.image}" 
-           alt="${child.name}" 
-           data-id="${key}" 
-           onclick="edit(event)">
-      <span class="info">${child.name}</span>
-    </li>
-  `;
-}
+window.addEventListener('load', function () {
+  switchPage('main');
+});
 
 function edit(event) {
   const id = event.target.dataset.id;
   window.location.href = `add.html?item=${encodeURIComponent(id)}`;
 }
 
-
-
 function switchPage(pageId) {
   const pages = document.querySelectorAll('.page');
   pages.forEach(page => {
     page.style.display = page.id === pageId ? 'block' : 'none';
   });
-}
-
-
-function home() {
-  switchPage('main');
 }
